511c2f4fdef9aaffcdb655d42410f6be442e9cb0
Arun Kirubarajan <kiruba@cis.upenn.edu>
2021-05-03 11:39:43 -0400

    Update

---
+++ turing.py
@@ -0,0 +1,28 @@
+import torch
+from flask import Flask, render_template, request, redirect, url_for
+from models import Model
+from data import CorpusDataset
+from decoding import top_k
+from bleu import list_bleu
+
+# defining hyperparameters
+NUM_EPOCHS = 1
+LEARNING_RATE = 0.01
+GRADIENT_NORM = 5
+EMBEDDING_DIM = 32
+TOP_K = 5
+HIDDEN_DIM = 32
+BATCH_SIZE = 16
+CHUNK_SIZE = 32
+TRAIN_PATH = "data/french.txt"
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dataset = CorpusDataset(TRAIN_PATH, CHUNK_SIZE, BATCH_SIZE)
+model = Model(EMBEDDING_DIM, HIDDEN_DIM, len(dataset.vocabulary), device)
+model.load_state_dict(torch.load('models/french.pt'))
+model.eval()
+
+ref = ['it is a white cat .',
+             'wow , this dog is huge .']
+hyp = ['it is a white kitten .',
+            'wowww , the dog is huge !']
